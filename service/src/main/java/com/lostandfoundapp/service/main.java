05cb7032108e48215621347eaf991f1ccdedd77c
Unknown <liso.seba@gmail.com>
2018-01-11 15:59:11 +0100

    spring REST

--- service/src/main/java/com/lostandfoundapp/service/main.java
+++ service/src/main/java/com/lostandfoundapp/service/main.java
@@ -9,46 +9,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
-
-import java.io.IOException;
-import java.net.URI;
-import java.util.Optional;
-
-
 public class main {
-
-    // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI;
-    public static final String protocol;
-    public static final Optional<String> host;
-    public static final String path;
-    public static final Optional<String> port;
-
-    static{
-        protocol = "http://";
-        host = Optional.ofNullable(System.getenv("HOSTNAME"));
-        port = Optional.ofNullable(System.getenv("PORT"));
-        path = "myapp";
-        BASE_URI = protocol + host.orElse("localhost") + ":" + port.orElse("8080") + "/" + path + "/";
-
-    }
-
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
-     */
-    public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in com.example.rest package
-        final ResourceConfig rc = new ResourceConfig().packages("com.lostandfoundapp.service");
-
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
 
     public static void main(String[] args) {
 
@@ -72,12 +33,6 @@ public class main {
 
             //Fake method for the time of playing with grizzly
             service.createFakeItemList();
-
-            final HttpServer server = startServer();
-            System.out.println(String.format("Jersey app started with WADL available at "
-                    + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-            System.in.read();
-            server.stop();
 
         }catch (IOException e){
             e.printStackTrace();
